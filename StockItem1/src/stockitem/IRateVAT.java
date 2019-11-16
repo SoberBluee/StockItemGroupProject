cea1ae473eb92f606567ca15f372c6ea2c8faa02
SoberBluee <49061190+SoberBluee@users.noreply.github.com>
2019-11-16 00:01:26 +0000

    Add files via upload

---
+++ StockItem1/src/stockitem/IRateVAT.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StockItem;
+
+/**
+ *
+ * @author Carlo
+ */
+/**
+ *
+ * @author e4-donovan
+ */
+public interface IRateVAT {
+    //Setting the VAT as a constant in the method to be called from the main class
+    public static double getVAT(){
+        return .175;
+    }
+    
+}
